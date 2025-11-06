51e522e2f3dd4dd537722f6282dcab43408a1803
prasanta-123 <prasantamallik195@gmail.com>
2025-11-06 11:59:11 +0530

    findelements

--- src/test/java/Advance_Xpath/Youtube_FindElement.java
+++ src/test/java/Advance_Xpath/Youtube_FindElement.java
@@ -1,10 +1,33 @@
 package Advance_Xpath;
 
+
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Youtube_FindElement {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.youtube.com/");
+		List<WebElement> Links = driver.findElements(By.xpath("//a"));
+		int count= Links.size();
+		System.out.println("numbers of links aviable:"+count);
+		for (int i = 0; i < count; i++) {
+			String allLinks = Links.get(i).getText();
+			System.out.println(allLinks);
+			Thread.sleep(100);
+			
+		}
+	
+		driver.quit();
+		
 	}
 
 }
