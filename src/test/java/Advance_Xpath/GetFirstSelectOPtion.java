b8b5e11850fe8b27792e3351aba0e3ea8381de16
prasanta-123 <prasantamallik195@gmail.com>
2025-11-11 09:31:33 +0530

    getfirstselectOPtion

---
+++ src/test/java/Advance_Xpath/GetFirstSelectOPtion.java
@@ -0,0 +1,34 @@
+package Advance_Xpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class GetFirstSelectOPtion {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demoapps.qspiders.com/ui/dropdown/multiSelect?sublist=1");
+			Thread.sleep(3000);
+			WebElement element = driver.findElement(By.id("select-multiple-native"));
+			Select s=new Select(element);
+			
+			Thread.sleep(1000);
+			s.selectByContainsVisibleText("Cotton ");
+            Thread.sleep(2000);
+            s.selectByVisibleText("Mens Casual Premium ...");
+            Thread.sleep(1000);
+            
+            String option = s.getFirstSelectedOption().getText();
+            System.out.println(option);
+            Thread.sleep(2000);
+			
+			driver.quit();
+			
+			
+	}
+
+}
