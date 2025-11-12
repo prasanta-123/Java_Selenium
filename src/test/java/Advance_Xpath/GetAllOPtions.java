c883048b13df9119779dce817b837e79b0ccf527
prasanta-123 <prasantamallik195@gmail.com>
2025-11-12 09:48:26 +0530

    getalloptions

--- src/test/java/Advance_Xpath/GetAllOPtions.java
+++ src/test/java/Advance_Xpath/GetAllOPtions.java
@@ -1,11 +1,12 @@
 package Advance_Xpath;
 
-import java.util.Iterator;
-import java.util.List;
+//import java.sql.Driver;
+//import java.util.Iterator;
+//import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebElement;	
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -18,25 +19,28 @@ public class GetAllOPtions {
 			Thread.sleep(3000);
 			WebElement element = driver.findElement(By.id("select-multiple-native"));
 			Select s=new Select(element);
-			List<WebElement> alloptions = s.getOptions();
-			int count = alloptions.size();
-			System.out.println(count);
-		for (int i = 0; i < count; i++) {
-			String options = alloptions.get(i).getText();
-			System.out.println(options);
+//			List<WebElement> alloptions = s.getOptions();
+//			int count = alloptions.size();
+//			System.out.println(count);
+//		for (int i = 0; i < count; i++) {
+//			String options = alloptions.get(i).getText();
+//			System.out.println(options);
+			 String options = s.getWrappedElement(). getText();
+			 System.out.println(options);
 			Thread.sleep(1000);
+			driver.quit();
 			
 			
 			
 		}
 	
-			
+		
         
           
             
-			driver.quit();
+		
 		
 
 	}
 
-}
+
