3906dedccd43dfdf4235560eab2e9a4402bf0cfa
prasanta-123 <prasantamallik195@gmail.com>
2025-11-13 11:48:10 +0530

    explicit

--- src/test/java/Synchronization/Explicit_Wait.java
+++ src/test/java/Synchronization/Explicit_Wait.java
@@ -4,9 +4,11 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Explicit_Wait {
@@ -16,12 +18,21 @@ public class Explicit_Wait {
 		WebDriver driver =new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://demoapps.qspiders.com/ui?scenario=1");
+		
 		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
 		wait.until(ExpectedConditions.titleContains("Box"));
+		
 		driver.findElement(By.xpath("//section[text()='Dropdown']")).click();
-		Thread.sleep(3000);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Multi Select")));
+		
+		
 		driver.findElement(By.linkText("Multi Select")).click();
-		System.out.println("done");
+		Thread.sleep(1000);
+		WebElement element=driver.findElement(By.id("select-multiple-native"));
+		Select  s=new Select(element);
+		String options=s.getWrappedElement().getText();
+		System.out.println(options);
+		System.out.println("DONE");
 	
 		
 		driver.quit();
