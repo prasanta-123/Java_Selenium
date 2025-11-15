3ecd8929f4ee328ceb9353c38655120029823f36
prasanta-123 <prasantamallik195@gmail.com>
2025-11-15 11:24:58 +0530

    hidden popups

--- src/test/java/Popups/Alert_popups.java
+++ src/test/java/Popups/Alert_popups.java
@@ -2,6 +2,7 @@ package Popups;
 
 import java.time.Duration;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,7 +19,7 @@ public class Alert_popups {
 		driver.findElement(By.xpath("//section[text()='Javascript']")).click();
 		driver.findElement(By.xpath("//td[text()='SAMSUNG Galaxy']/preceding-sibling::td/child::input")).click();
 		driver.findElement(By.id("deleteButton")).click();
-		org.openqa.selenium.Alert a = driver.switchTo().alert();
+		Alert a = driver.switchTo().alert();
 		Thread.sleep(3000);
 		System.out.println(a.getText());
 		a.accept();
---
+++ src/test/java/Popups/Hidden_popups.java
@@ -0,0 +1,35 @@
+package Popups;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Hidden_popups {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.careinsurance.com/rhicl/proposalcp/renew/index-care");
+		driver.findElement(By.id("policynumber")).sendKeys("123");
+		driver.findElement(By.id("dob")).click();
+		WebElement month = driver.findElement(By.xpath("//select[@aria-label='Select month']"));
+		Select s=new Select(month);
+		s.selectByIndex(5);
+		WebElement year =driver.findElement(By.xpath("//select[@aria-label='Select year']"));
+		Select s1=new Select(year);
+		s1.selectByVisibleText("2010");
+		driver.findElement(By.linkText("4")).click();
+		Thread.sleep(3000);
+		driver.quit();
+		
+		
+	
+
+	}
+
+}
