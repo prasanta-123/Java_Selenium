163bcadfccdaf535dd592a9fe86943c9571f044d
prasanta-123 <prasantamallik195@gmail.com>
2025-11-15 09:46:24 +0530

    alert

--- src/test/java/Popups/Alert_popups.java
+++ src/test/java/Popups/Alert_popups.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class Alert {
+public class Alert_popups {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver=new ChromeDriver();
