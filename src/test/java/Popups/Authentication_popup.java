ef9d350911fa43bc53c5bf57a7520c1f2cba51fe
prasanta-123 <prasantamallik195@gmail.com>
2025-11-17 09:47:17 +0530

    authentication

--- src/test/java/Popups/Authentication_popup.java
+++ src/test/java/Popups/Authentication_popup.java
@@ -10,6 +10,7 @@ public class Authentication_popup {
 		driver.manage().window().maximize();
 		driver.get("https://admin:admin@basic-auth-git-main-shashis-projects-4fa03ca5.vercel.app/");
 		Thread.sleep(3000);
+		System.out.println("authentication handled..");
 		driver.quit();
 	
 
