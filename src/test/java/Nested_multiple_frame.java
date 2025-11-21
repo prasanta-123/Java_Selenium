810da6679a8a5be8b740fbfd9294d7f33bee3cbb
prasanta-123 <prasantamallik195@gmail.com>
2025-11-21 10:26:35 +0530

    nested_multiple

---
+++ src/test/java/Nested_multiple_frame.java
@@ -0,0 +1,32 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Nested_multiple_frame {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://demoapps.qspiders.com/ui/frames/nestedWithMultiple?sublist=3");
+		Thread.sleep(2000);
+		driver.switchTo().frame(0);
+		Thread.sleep(2000);
+		driver.switchTo().frame(0);
+		Thread.sleep(2000);
+		driver.switchTo().frame(0);
+		Thread.sleep(2000);
+		driver.findElement(By.id("email")).sendKeys("prasanta@gmail.com");
+		Thread.sleep(2000);
+		driver.switchTo().defaultContent();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//section[.='Synchronization']")).click();
+		Thread.sleep(5000);
+		driver.quit();
+	
+		
+	}
+
+}
