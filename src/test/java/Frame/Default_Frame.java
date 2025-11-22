e8ee3aeb4aae8bc86942adf39f548b83e313a2cf
prasanta-123 <prasantamallik195@gmail.com>
2025-11-22 09:26:19 +0530

    mouse hover

--- src/test/java/Frame/Default_Frame.java
+++ src/test/java/Frame/Default_Frame.java
@@ -10,17 +10,18 @@ public class Default_Frame {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-			WebDriver driver = new  ChromeDriver();
+		WebDriver driver = new  ChromeDriver();
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 			driver.get("https://demoapps.qspiders.com/ui/frames?sublist=0");
 			Thread.sleep(2000);
 			driver.switchTo().frame(0);
-			driver.findElement(By.id("username")).sendKeys("deepak");
-			driver.findElement(By.id("password")).sendKeys("deepak123");
+			driver.findElement(By.id("username")).sendKeys("prasanta");
+			driver.findElement(By.id("password")).sendKeys("prasanta");
 			driver.findElement(By.id("submitButton")).click();
 			Thread.sleep(2000);
 			driver.quit();
+		
 	}
 
 }
---
+++ src/test/java/Mouse_Action/Mouse_Hoover.java
@@ -0,0 +1,33 @@
+package Mouse_Action;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Mouse_Hoover {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.vtiger.com/");
+		WebElement element = driver.findElement(By.id("companyDropdown"));
+		Actions a=new Actions(driver);
+		a.moveToElement(element).perform();
+		Thread.sleep(3000);
+		driver.findElement(By.linkText("Contact Us")).click();
+		String details = driver.findElement(By.xpath("(//div[@class='text-reset text-decoration-none'])[2]")).getText();
+		System.out.println(details);
+		driver.quit();
+		
+		
+		
+		
+
+	}
+
+}
